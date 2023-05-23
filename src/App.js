aaf66783fcd2e01859cedabba379379ee77bca85
Maxime Vincent <git@maximevincent.com>
2023-05-23 17:19:30 -0400

    Add Firebase config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVLIgB7np4kEi76X4xVappSfNK4rfh-TE",
+  authDomain: "chaquevoix.firebaseapp.com",
+  projectId: "chaquevoix",
+  storageBucket: "chaquevoix.appspot.com",
+  messagingSenderId: "42107547318",
+  appId: "1:42107547318:web:9a0078896d50891fc30017",
+  measurementId: "G-LRRLRESJ2R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
