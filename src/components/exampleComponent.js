1ff7c3a5e81be4e6d69f866dae3ae31a0d4410d8
Maxime Vincent <git@maximevincent.com>
2023-05-23 23:19:49 -0400

    Create example component that gets firestore data

---
+++ src/components/exampleComponent.js
@@ -0,0 +1,35 @@
+import '../App.css';
+
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../firebaseConfig';
+import { useState, useEffect } from 'react';
+
+function ExampleComponent() {
+
+    const [loading, setLoading] = useState(false);
+    const [schools, setSchools] = useState();
+
+    useEffect(() => {
+
+        const loadSchool = async () => {
+            setLoading(true);
+
+            const schoolsDoc = doc(db, "/schools/CG0bz6uHYgPxm3T4EKm4")
+            const document = await getDoc(schoolsDoc);
+            console.log(document.data())
+
+            setSchools(JSON.stringify(document.data()));
+            setLoading(false);
+        }
+
+        loadSchool();
+    }, []);
+
+    return (
+        <div>
+            {schools}
+        </div>
+    );
+}
+
+export default ExampleComponent;
