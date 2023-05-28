b500a2248a644eb954b14fd3f17e091d5a8dbb64
Maxime V <git@maximevincent.com>
2023-05-28 02:32:42 -0400

    Configure theme with toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,40 @@ import Login from './pages/auth/Login';
 import Account from './pages/Account';
 import Register from './pages/auth/Register';
 import LinkUser from './pages/auth/LinkUserToAccount';
+import { useState, useEffect } from 'react';
+import { ConfigProvider, theme, Button, Layout } from "antd";
 
 function App() {
+  // Dark theme taken and modified from https://betterprogramming.pub/how-to-toggle-dark-theme-with-ant-design-5-0-eb68552f62b8
+  
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+    document.cookie = `ManuallySetLightMode=${isDarkMode}`
+  };
   return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/link_user" element={<LinkUser />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+      }}>
+      <Layout style={{ minHeight: "100vh", color: isDarkMode ? "#f0f0f0" : "#1f1f1f" }}>
+          <Button onClick={handleClick}>
+            Change Theme to {isDarkMode ? "Light" : "Dark"}
+          </Button>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/link_user" element={<LinkUser />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </BrowserRouter>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
