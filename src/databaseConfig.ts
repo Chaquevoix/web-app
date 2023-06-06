d0d6eff94bfd9cd60306c7d9d1de07b9e439b891
Maxime Vincent <git@maximevincent.com>
2023-06-06 15:14:28 -0400

    Add back firebase config for compatibility reasons

--- src/databaseConfig.ts
+++ src/databaseConfig.ts
@@ -1,32 +1,33 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore, doc, getDoc } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAVLIgB7np4kEi76X4xVappSfNK4rfh-TE",
-//   authDomain: "chaquevoix.firebaseapp.com",
-//   projectId: "chaquevoix",
-//   storageBucket: "chaquevoix.appspot.com",
-//   messagingSenderId: "42107547318",
-//   appId: "1:42107547318:web:9a0078896d50891fc30017",
-//   measurementId: "G-LRRLRESJ2R"
-// };
-
-// // Initialize Firebase
-// const firebase = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(firebase);
-// export const db = getFirestore(firebase);
-// export const auth = getAuth(firebase);
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("https://bqjhuqhzmxhplftvtibn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxamh1cWh6bXhocGxmdHZ0aWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwMjYzOTcsImV4cCI6MjAwMTYwMjM5N30.IhV3nqZFlhDxJTWincaz3XlYlIra8HRZ-QPfrC8Vd4g");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const db = supabase;
-export const auth = supabase.auth;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVLIgB7np4kEi76X4xVappSfNK4rfh-TE",
+  authDomain: "chaquevoix.firebaseapp.com",
+  projectId: "chaquevoix",
+  storageBucket: "chaquevoix.appspot.com",
+  messagingSenderId: "42107547318",
+  appId: "1:42107547318:web:9a0078896d50891fc30017",
+  measurementId: "G-LRRLRESJ2R"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+export const db = getFirestore(firebase);
+export const auth = getAuth(firebase);
+
+
+export const supabase = createClient("https://bqjhuqhzmxhplftvtibn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxamh1cWh6bXhocGxmdHZ0aWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwMjYzOTcsImV4cCI6MjAwMTYwMjM5N30.IhV3nqZFlhDxJTWincaz3XlYlIra8HRZ-QPfrC8Vd4g");
+
+export const supabaseAuth = supabase.auth;
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Checkbox, Form, Input, message } from "antd";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { collection, query, where, getDocs, limit } from "firebase/firestore";
-import { auth, db } from "../databaseConfig";
+import { supabase, supabaseAuth } from "../databaseConfig";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 import { useNavigate } from "react-router-dom";
@@ -11,18 +11,30 @@ import { useEffect, useState } from "react";
 
 function Account() {
   const navigate = useNavigate();
-  const [authUserData, authLoading, authError] = useAuthState(auth);
+  // const [authUserData, authLoading, authError] = useAuthState(auth);
+  const [dbValues, setDbValues] = useState<any>();
 
-  const [dbValues, dbLoading, dbError, dbSnapshot] = useCollectionDataOnce<any>(
-    query(
-      collection(db, 'users'),
-      where(
-        'associated_user_account',
-        "==",
-        authUserData ? authUserData.uid : "no_user_found"),
-      limit(1)
-    )
-  );
+  // const [dbValues, dbLoading, dbError, dbSnapshot] = useCollectionDataOnce<any>(
+  //   query(
+  //     collection(db, 'users'),
+  //     where(
+  //       'associated_user_account',
+  //       "==",
+  //       authUserData ? authUserData.uid : "no_user_found"),
+  //     limit(1)
+  //   )
+  // );
+
+  console.log(supabaseAuth.getUser())
+
+  useEffect(() => {
+    getUserDataFromAssociatedUserAccount();
+  }, []);
+
+  async function getUserDataFromAssociatedUserAccount() {
+    const { data } = await supabase.from("user-profile").select("*").eq("associated-user-account", supabaseAuth.getUser);
+    setDbValues(data);
+  }
 
   const { t } = useTranslation();
 
@@ -34,7 +46,7 @@ function Account() {
     //   .catch((error) => {
     //     message.error(t('global.generic_error') + error);
     //   });
-    auth.signOut()
+    supabaseAuth.signOut()
   };
 
   const handleNavigateToLinkUser = () => {
@@ -42,45 +54,45 @@ function Account() {
     return true;
   }
 
-  // validation
-  if (authLoading && dbLoading) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+  // // validation
+  // if (authLoading && dbLoading) {
+  //   return (
+  //     <div>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   );
+  // }
 
-  if (!authUserData) {
-    return (
-      <div>
-        <h1>{t('global.user_not_logged_in')}</h1>
-      </div>
-    );
-  }
+  // if (!authUserData) {
+  //   return (
+  //     <div>
+  //       <h1>{t('global.user_not_logged_in')}</h1>
+  //     </div>
+  //   );
+  // }
 
-  if (!dbValues || !dbSnapshot?.docs[0]) {
-    return (
-      <div>
-        <h1>{t('pages.account.no_profile_linked')}</h1>
-        <Button type="default" onClick={handleNavigateToLinkUser}>
-          Link user
-        </Button>
-      </div>
-    );
-  }
+  // if (!dbValues || !dbSnapshot?.docs[0]) {
+  //   return (
+  //     <div>
+  //       <h1>{t('pages.account.no_profile_linked')}</h1>
+  //       <Button type="default" onClick={handleNavigateToLinkUser}>
+  //         Link user
+  //       </Button>
+  //     </div>
+  //   );
+  // }
 
-  const document = dbSnapshot?.docs[0].data();
+  // const document = dbSnapshot?.docs[0].data();
 
   // content
   return (
     <div>
-      <h1>{t('pages.account.logged_in_as')} {authUserData.email}</h1>
+      {/* <h1>{t('pages.account.logged_in_as')} {authUserData.email}</h1> */}
       <Button type="primary" onClick={logOutButton}>
         Log Out
       </Button>
       <div>
-        <h1>Welcome back, {document.first_name}!</h1>
+        {/* <h1>Welcome back, {document.first_name}!</h1> */}
       </div>
     </div>
   );
