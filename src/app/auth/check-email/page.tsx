3178275844ceef27a03d28dafd63a9a9e6f72f10
Maxime Vincent <git@maximevincent.com>
2024-04-29 20:42:24 -0400

    remove apostrophe

--- src/app/auth/check-email/page.tsx
+++ src/app/auth/check-email/page.tsx
@@ -6,7 +6,7 @@ export default function Register() {
     return (
         <main>
             <h1>Check your email inbox!</h1>
-            <h2>We've sent you a verification email.</h2>
+            <h2>We have sent you a verification email.</h2>
         </main>
     );
 }
